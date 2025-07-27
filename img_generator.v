87b8b9c9629cfee9bfaadcd22d926748c49ed5c5
Pro-NerdX <highLevelNerdAndActivist@proton.me>
2025-07-27 16:44:10 +0200

    (img_generator) zones fully fixed

--- img_generator.v
+++ img_generator.v
@@ -21,11 +21,11 @@
 `define BALL_CENTER_OFFSET 5
 
 `define HIT_ZONE_1    0
-`define HIT_ZONE_2   13
-`define HIT_ZONE_3   25
+`define HIT_ZONE_2   11
+`define HIT_ZONE_3   23
 `define HIT_ZONE_4   35
 `define HIT_ZONE_5   47
-`define HIT_ZONE_MAX 60
+`define HIT_ZONE_MAX 59
 
 module img_generator (
     input wire CLOCK_25,
