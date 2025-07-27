a25a5139e688186f1e666654f7ff7462e0950dc7
Janluucas <jabu00001@uni-saarland.de>
2025-07-27 19:16:56 +0200

    changed for active low

--- rotary_encoder.v
+++ rotary_encoder.v
@@ -9,8 +9,8 @@ module rotary_encoder (
     output wire down,
     output wire button
 );
-    reg a_up_first = 0;
-    reg b_up_first = 0;
+    reg a_low_first = 0;
+    reg b_low_first = 0;
 
     reg up_r = 0;
     reg down_r = 0;
@@ -21,32 +21,32 @@ module rotary_encoder (
 
     always @(posedge clk) begin
         if (!signal_sent) begin
-            if (!a_up_first && !b_up_first) begin
-                if (in_a) begin
-                    a_up_first <= 1;
-                end else if (in_b) begin
-                    b_up_first <= 1;
+            if (!a_low_first && !b_low_first) begin
+                if (!in_a) begin
+                    a_low_first <= 1;
+                end else if (!in_b) begin
+                    b_low_first <= 1;
                 end
                 up_r <= 0;
                 down_r <= 0;
-            end else if (a_up_first) begin
-                if (in_b) begin
+            end else if (a_low_first) begin
+                if (!in_b) begin
                     up_r <= 1;
-                    a_up_first <= 0;
-                    b_up_first <= 0;
+                    a_low_first <= 0;
+                    b_low_first <= 0;
                     signal_sent <= 1;
                 end
-            end else if (b_up_first) begin
-                if (in_a) begin
+            end else if (b_low_first) begin
+                if (!in_a) begin
                     down_r <= 1;
-                    a_up_first <= 0;
-                    b_up_first <= 0;
+                    a_low_first <= 0;
+                    b_low_first <= 0;
                     signal_sent <= 1;
                 end
             end
         end else begin
-            a_up_first <= 0;
-            b_up_first <= 0;
+            a_low_first <= 0;
+            b_low_first <= 0;
             up_r <= 0;
             down_r <= 0;
             signal_sent <= 0;
