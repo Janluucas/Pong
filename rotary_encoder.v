5974d9d5ee7b2939f3a49de0c0b9422b537e3705
Pro-NerdX <highLevelNerdAndActivist@proton.me>
2025-08-01 16:49:57 +0200

    (rotary_encoder) bulldozed

--- rotary_encoder.v
+++ rotary_encoder.v
@@ -14,38 +14,27 @@ module rotary_encoder (
 
     reg up_r = 0;
     reg down_r = 0;
-    assign up = (dead_zone == 32'hFFFFFFFF) ? up_r : 0;
-    assign down = (dead_zone == 32'hFFFFFFFF) ? down_r : 0;
+    reg out = 0;
+    assign up = out? up_r : 0;
+    assign down = out? down_r : 0;
 
-    reg signal_sent = 0;
-
-    reg[31:0] dead_zone = 32'b0;
-
-    always @(posedge clk) begin
-        casez ({in_a, in_b, a_low_first, b_low_first, signal_sent})
-            2'b??_??_1: begin
-                dead_zone <= dead_zone + 1'b1;
-                if (dead_zone == 32'hFFFFFFFF) begin
-                    signal_sent <= 0;
-                    up_r <= 0;
-                    down_r <= 0;
-                    a_low_first <= 0;
-                    b_low_first <= 0;
-                end
-            end
-
-            2'b0?_01_0: begin
-                up_r <= 1;
-                signal_sent <= 1;
-            end
-            2'b?0_10_0: begin
-                down_r <= 1;
-                signal_sent <= 1;
-            end
-
-            2'b01_00_0: a_low_first <= 1;
-            2'b10_00_0: b_low_first <= 1;
-        endcase
+    always @(negedge in_a) begin 
+        if (in_b) begin
+            up_r = 1;
+            down_r = 0;
+        end else begin
+            up_r = 0;
+            down_r = 1;
+        end
     end
 
+    always @(posedge clk) begin
+        out <= 1;
+    end
+
+    always @(negedge clk) begin
+        out <= 0;
+        up_r <= 0;
+        down_r <= 0;
+    end
 endmodule
