f6c44bcb8abe21b84f890de75f4ea07aa4facb27
Pro-NerdX <highLevelNerdAndActivist@proton.me>
2025-08-07 19:12:20 +0200

    (scores) score gets reseted; ball gets colored in color of last winner; player colors outsourced to global_symbols.vh for quick change of player colors

--- img_generator.v
+++ img_generator.v
@@ -27,25 +27,6 @@ module img_generator (
 
     reg pause_active_low = 0;   // yes, by default the game is supposed to be paused
 
-/*    always @(posedge BALL_CLOCK) begin
-        // key1 == continue
-        if (key1 == 0) begin
-            pause_active_low <= 1;
-
-        // key0 == pause
-        end else if (key0 == 0) begin
-            pause_active_low <= 0
-        end
-    end*/
-
-    /*reg[1:0] mode;
-    // Animations on FPGA LEDs
-    animation a0(
-        .BALL_CLOCK(BALL_CLOCK),
-        .mode(mode),
-        .
-    );*/
-
     // Player Logic
     always @(posedge CLOCK_25) begin
         if (pause_active_low == 0) begin
@@ -92,27 +73,27 @@ module img_generator (
     reg[2:0] current_ball_x_movement = 4;
     reg[2:0] current_ball_y_movement = 0;
 
-
+    reg[2:0] ball_color = 3'b111;
 
     assign color = (
         // Draw Ball
         x >= ball_x_pos && x <= (ball_x_pos + `BALL_RADIUS) &&
         y >= ball_y_pos && y <= (ball_y_pos + `BALL_RADIUS)
-    ) ? 3'b111 : (
+    ) ? ball_color : (
         // Draw Score 1
         score_1_out
-    ) ? 3'b001 : (
+    ) ? `PLAYER_1_COLOR : (
         // Draw Score 2
         score_2_out
-    ) ? 3'b100 : (
+    ) ? `PLAYER_2_COLOR : (
         // Draw Player 1
         x >= `PLAYER_1_X_POS && x <= (`PLAYER_1_X_POS + `PLAYER_WIDTH) &&
         y >= player_1_y_pos && y <= (player_1_y_pos + `PLAYER_HEIGHT)
-    ) ? 3'b001 : (
+    ) ? `PLAYER_1_COLOR : (
         // Draw Player 2
         x >= `PLAYER_2_X_POS && x <= (`PLAYER_2_X_POS + `PLAYER_WIDTH) &&
         y >= player_2_y_pos && y <= (player_2_y_pos + `PLAYER_HEIGHT)
-    ) ? 3'b100 : 3'b000 | 3*{(x == 640 || x == 1 || y == 480 || y == 1) ? 3'b010 : 3'b000};
+    ) ? `PLAYER_2_COLOR : 3'b000 | 3*{(x == 640 || x == 1 || y == 480 || y == 1) ? 3'b010 : 3'b000};
 
     reg[11:0] player_1_y_pos = `INITIAL_PLAYER_Y_POS;
     reg[11:0] player_2_y_pos = `INITIAL_PLAYER_Y_POS;
@@ -188,7 +169,13 @@ module img_generator (
                     current_ball_y_movement <= 3;
                 end else begin
                     // MISS => Player 2 scores
-                    score_player_2 <= score_player_2 + 1;
+                    if (score_player_2 == 7) begin
+                        score_player_2 <= 0;
+                        score_player_1 <= 0;
+                        ball_color <= `PLAYER_2_COLOR;
+                    end else begin
+                        score_player_2 <= score_player_2 + 1;
+                    end
                     miss_indicator <= 1;
                 end
 
@@ -203,7 +190,13 @@ module img_generator (
                     current_ball_y_movement <= 3;
                 end else begin
                     // MISS => Player 2 scores
-                    score_player_2 <= score_player_2 + 1;
+                    if (score_player_2 == 7) begin
+                        score_player_2 <= 0;
+                        score_player_1 <= 0;
+                        ball_color <= `PLAYER_2_COLOR;
+                    end else begin
+                        score_player_2 <= score_player_2 + 1;
+                    end
                     miss_indicator <= 1;
                 end
 
@@ -273,7 +266,13 @@ module img_generator (
                     current_ball_y_movement <= 3;
                 end else begin
                     // MISS => Player 1 scores
-                    score_player_1 <= score_player_1 + 1;
+                    if (score_player_1 == 7) begin
+                        score_player_1 <= 0;
+                        score_player_2 <= 0;
+                        ball_color <= `PLAYER_1_COLOR;
+                    end else begin
+                        score_player_1 <= score_player_1 + 1;
+                    end
                     miss_indicator <= 1;
                 end
 
@@ -288,7 +287,13 @@ module img_generator (
                     current_ball_y_movement <= 3;
                 end else begin
                     // MISS => Player 1 scores
-                    score_player_1 <= score_player_1 + 1;
+                    if (score_player_1 == 7) begin
+                        score_player_1 <= 0;
+                        score_player_2 <= 0;
+                        ball_color <= `PLAYER_1_COLOR;
+                    end else begin
+                        score_player_1 <= score_player_1 + 1;
+                    end
                     miss_indicator <= 1;
                 end
 
