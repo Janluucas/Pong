12fe28915b156985878edb619bf3b4d68a4c0241
Janluucas <b.janlucas1@gmail.com>
2025-08-11 17:42:11 +0200

    keypad modul von lecture

--- keypad.v
+++ keypad.v
@@ -1,34 +1,61 @@
-module keypad (
-	input wire clk,
-	input wire [3:0] cols,
-	output reg [3:0] rows,
-	output reg [3:0] keycode,
-	output reg keypressed
+module keypad(
+    input  wire       clk,
+    input  wire [3:0] cols,
+    output reg  [3:0] rows,
+    output reg  [3:0] keycode
 );
-	reg [1:0] state;
+
+	/*********************************************************************
+	* Anmerkung:
+	* Die Input-Ports sollten mit einem internen Pull-Up versehen werden.
+	* Dies kann in der 'assignments' hinzugefügt werden
+	*
+	* set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to GPIO_023
+	* set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to GPIO_021
+	* set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to GPIO_019
+	* set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to GPIO_017
+	*********************************************************************/
+
+	// Scan-Zyklus über Zeilen
+	// Da wir nur Pull-Ups verwenden können, wird das invertierte
+	// Tastendrucksignal gemessen, d.h. alle Ausgänge bis auf einen
+	// sind 1 in rows und in cols wird die entsprechende Leitung
+	// auf 0 gezogen.
+	reg [1:0] row_sel = 2'd0;
+	reg [9:0] cnt = 0;		// Debounce-Timer
 
 	always @(posedge clk) begin
-		state <= state + 1'b1;
-		rows <= ~(4'b0001 << state);
+		cnt <= cnt + 1;
+		// Debouncing Intervall
+		if (cnt[9]) begin
+			row_sel <= row_sel + 1'b1;		// row to be scanned
+			rows    <= ~(4'b0001 << row_sel);		// set rows
+		end
+	end
 
+
+	// Zuordnung der gedrückten Taste aus cols
+	// mittels kombinatorische Multiplexer- und sequentieller
+	// keycode-Schaltung
+	always @(posedge clk) begin
 		case ({~rows, ~cols})
-			8'b0001_0001: begin keypressed <= 1; keycode <= 4'd1; 	end		// '1'
-			8'b0001_0010: begin keypressed <= 1; keycode <= 4'd2; 	end 	// '2'
-			8'b0001_0100: begin keypressed <= 1; keycode <= 4'd3; 	end		// '3'
-			8'b0001_1000: begin keypressed <= 1; keycode <= 4'd10; 	end		// 'A'
-			8'b0010_0001: begin keypressed <= 1; keycode <= 4'd4; 	end		// '4'
-			8'b0010_0010: begin keypressed <= 1; keycode <= 4'd5; 	end		// '5'
-			8'b0010_0100: begin keypressed <= 1; keycode <= 4'd6; 	end		// '6'
-			8'b0010_1000: begin keypressed <= 1; keycode <= 4'd11; 	end		// 'B'
-			8'b0100_0001: begin keypressed <= 1; keycode <= 4'd7; 	end		// '7'
-			8'b0100_0010: begin keypressed <= 1; keycode <= 4'd8; 	end		// '8'
-			8'b0100_0100: begin keypressed <= 1; keycode <= 4'd9; 	end		// '9'
-			8'b0100_1000: begin keypressed <= 1; keycode <= 4'd12; 	end		// 'C'
-			8'b1000_0001: begin keypressed <= 1; keycode <= 4'd14; 	end		// '*'
-			8'b1000_0010: begin keypressed <= 1; keycode <= 4'd0; 	end		// '0'
-			8'b1000_0100: begin keypressed <= 1; keycode <= 4'd15;	end		// '#'
-			8'b1000_1000: begin keypressed <= 1; keycode <= 4'd13; 	end		// 'D'
-			default:      begin keypressed <= 0; keycode <= keycode; 	end
+			8'b0001_0001: keycode <= 4'd1;	// '1'
+			8'b0001_0010: keycode <= 4'd2; 	// '2'
+			8'b0001_0100: keycode <= 4'd3;	// '3'
+			8'b0001_1000: keycode <= 4'd10;	// 'A'
+			8'b0010_0001: keycode <= 4'd4;	// '4'
+			8'b0010_0010: keycode <= 4'd5;	// '5'
+			8'b0010_0100: keycode <= 4'd6;	// '6'
+			8'b0010_1000: keycode <= 4'd11;	// 'B'
+			8'b0100_0001: keycode <= 4'd7;	// '7'
+			8'b0100_0010: keycode <= 4'd8;	// '8'
+			8'b0100_0100: keycode <= 4'd9;	// '9'
+			8'b0100_1000: keycode <= 4'd12;	// 'C'
+			8'b1000_0001: keycode <= 4'd14;	// '*'
+			8'b1000_0010: keycode <= 4'd0;	// '0'
+			8'b1000_0100: keycode <= 4'd15;	// '#'
+			8'b1000_1000: keycode <= 4'd13;	// 'D'
+			default:      keycode <= keycode;
 		endcase
 	end
 
