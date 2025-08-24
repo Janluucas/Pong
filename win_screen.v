795aeb2a8bdd28d6eaed99f3ba4bf17427c5b779
Janluucas <b.janlucas1@gmail.com>
2025-08-24 20:29:13 +0200

    commented win_screen

--- win_screen.v
+++ win_screen.v
@@ -1,20 +1,22 @@
 `include "global_symbols.vh"
 
+// Module to display the "WIN" text for the winning player on the screen
 module win_screen (
-    input wire clk,
+    input wire clk,                // Clock signal
 
-    input wire[11:0] x,
-    input wire[11:0] y,
+    input wire[11:0] x,            // Current pixel x-coordinate
+    input wire[11:0] y,            // Current pixel y-coordinate
 
-    input wire[2:0] winner,
+    input wire[2:0] winner,        // Indicates which player won
 
-    output wire out
+    output wire out                // Output: 1 if current pixel is part of the win text, 0 otherwise
 );
     reg out_r;
     assign out = out_r;
 
     always @(posedge clk) begin
         case (winner)
+            // If Player 1 wins, draw "WIN" at Player 1's side
             `PLAYER_1_COLOR: begin
                 out_r <= ( // DRAW 'W'
                     (`WIN_TEXT_PLAYER_1_X_POS <= x) &&
@@ -74,6 +76,7 @@ module win_screen (
                 );
             end
 
+            // If Player 2 wins, draw "WIN" at Player 2's side
             `PLAYER_2_COLOR: begin
                 out_r <= ( // DRAW 'W'
                     (`WIN_TEXT_PLAYER_2_X_POS <= x) &&
@@ -133,6 +136,7 @@ module win_screen (
                 );
             end
 
+            // Default: no output
             default: out_r <= 0; 
         endcase
     end
